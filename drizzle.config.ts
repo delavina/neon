49e3a2568293442111a920ad2651d933a237e524
delavinamedia <koeppen.h@gmail.com>
2024-12-26 15:56:41 +0100

    fine-tuning

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,18 +1,19 @@
-import type { Config } from "drizzle-kit";
-import * as dotenv from "dotenv";
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 if (!process.env.NEON_DATABASE_URL) {
-  throw new Error("Missing NEON_DATABASE_URL environment variable");
+  throw new Error('Missing NEON_DATABASE_URL environment variable');
 }
 
 export default {
   schema: './src/app/db/schema.ts',
-  out: "./src/app/db/migrations",
-  dialect: "postgresql",
+  out: './src/app/db/migrations',
+  dialect: 'postgresql',
   dbCredentials: {
     url: process.env.NEON_DATABASE_URL,
   },
+  verbose: true,
   strict: true,
 } satisfies Config;
